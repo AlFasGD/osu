9bcb8bf02c2e98e44017cee09a5421591d100924
AlFasGD <kalfas.alex@gmail.com>
2018-07-08 23:04:54 +0300

    Revert "Update framework, change textbox colour"
    This reverts commit b2e666a4a71c287ad2380a050ddb5a1e2982f2e7.

--- osu.Game/Screens/Edit/Screens/Setup/Components/SetupOsuTextBox.cs
+++ osu.Game/Screens/Edit/Screens/Setup/Components/SetupOsuTextBox.cs
@@ -2,9 +2,8 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
 using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
 
 namespace osu.Game.Screens.Edit.Screens.Setup.Components
@@ -17,11 +16,6 @@ namespace osu.Game.Screens.Edit.Screens.Setup.Components
         private void load(OsuColour osuColour)
         {
             BorderColour = osuColour.Blue;
-        }
-
-        protected override Drawable GetDrawableCharacter(char c)
-        {
-            return new OsuSpriteText { Text = c.ToString(), Colour = BorderColour, TextSize = CalculatedTextSize };
         }
     }
 }
